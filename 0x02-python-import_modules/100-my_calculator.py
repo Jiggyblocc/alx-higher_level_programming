6e1a7609b6d83a8c4454cd39eb2214c1e813b5ac
edafe ajirigit config --global user.name edafe <edafeajiri5@gmail.comgit config --global user.email edafeajiri5@gmail.com>
2022-12-07 13:05:15 -0800

    task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-def main(argv):
-    argc = len(argv)
-    ops = {
-        '+': calculator_1.add,
-        '-': calculator_1.sub,
-        '*': calculator_1.mul,
-        '/': calculator_1.div,
-    }
-    if argc != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
-    if op not in '+-*/':
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    res = ops[op](a, b)
-    print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
+    argc = len(argv) - 1
+    if argc != 3:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    elif argv[2] == '+':
+        result = add(int(argv[1]), int(argv[3]))
+    elif argv[2] == '-':
+        result = sub(int(argv[1]), int(argv[3]))
+    elif argv[2] == '*':
+        result = mul(int(argv[1]), int(argv[3]))
+    elif argv[2] == '/':
+        result = div(int(argv[1]), int(argv[3]))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-    if __name__ == '__main__':
-        from sys import argv, exit
-        import calculator_1
-        main(argv)
+    print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
